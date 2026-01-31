fcb2c0f5f9f2c8f6eeecb246c0d30e8399fcbb26
kiExaa <khairulrizki2005@gmail.com>
2026-01-31 13:57:40 +0700

    Update proses_edit.php

--- biodata/siswa/proses_edit.php
+++ biodata/siswa/proses_edit.php
@@ -11,14 +11,49 @@
     $alamat = $_POST['alamat'];
     $email = $_POST['email'];
     $jk = $_POST['jns_kelamin'];
+    $gelombang = $_POST['gelombang'];
     $jur = $_POST['jurusan'];
+    $nama_foto = $_FILES['foto']['name'];
+    $tmp_foto = $_FILES['foto']['tmp_name'];
 
-    #3. Query Insert (proses tambah data)
-    $query = "UPDATE biodata SET nama='$nama', nisn='$nisn', tempat_lahir='$tp_lahir', 
-    tgl_lahir='$tgl_lahir', alamat='$alamat', email='$email', jns_kelamin='$jk',  jurusan='$jur' 
+    if($nama_foto !=""){
+        $qry = "SELECT * FROM biodata WHERE id='$id'";
+        $hapus_foto = mysqli_query($koneksi,query: $qry);
+        $data = mysqli_fetch_array($hapus_foto);
+        $nama_foto_hapus = $data['foto'];
+        $lokasi_foto = "../fotosiswa/$nama_foto_hapus";
+        if(file_exists($lokasi_foto)){
+          unlink($lokasi_foto);
+        }
+
+
+        $query = "UPDATE biodata SET nama='$nama', nisn='$nisn', tempat_lahir='$tp_lahir', 
+    tgl_lahir='$tgl_lahir', alamat='$alamat', email='$email', jns_kelamin='$jk',gelombangs_id='$gelombang',  jurusans_id='$jur', foto='$nama_foto'
     WHERE id='$id'";
 
+    
+
+     #hapusfoto
+    //  $lokasi_foto = "../fotosiswa/$nama_foto";
+    //  if(file_exists($lokasi_foto)){
+    //     unlink($lokasi_foto);
+    // }
+
+    move_uploaded_file($tmp_foto,"../fotosiswa/$nama_foto");
+
+
+    }else{
+    #3. Query Insert (proses tambah data)
+    $query = "UPDATE biodata SET nama='$nama', nisn='$nisn', tempat_lahir='$tp_lahir', 
+    tgl_lahir='$tgl_lahir', alamat='$alamat', email='$email', jns_kelamin='$jk',gelombangs_id='$gelombang',  jurusans_id='$jur' 
+    WHERE id='$id'";
+    }
+
+   
     $tambah = mysqli_query($koneksi,$query);
+    // $lokasi_foto = "./fotosiswa/$nama_foto";
+
+
 
     #4. Jika Berhasil triggernya apa? (optional)
     if($tambah){
@@ -26,4 +61,4 @@
     }else{
         echo "Data Gagal ditambah";
     }
-?>z
+?>
